5734279730f8dac12333425b9fac5fd685c674f8
Pana <pan.wang@theseus.app>
2018-09-08 15:51:44 +0800

    add restart task

--- backend/task/task.go
+++ backend/task/task.go
@@ -14,16 +14,19 @@ func main()  {
 	viper.SetConfigName("config")
 	viper.AddConfigPath("~/go/src/github.com/iost-official/explorer/backend")
 	viper.AddConfigPath(".")
-	err := viper.ReadInConfig() // Find and read the config file
-	if err != nil { // Handle errors reading the config file
+	err := viper.ReadInConfig()
+	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
 	db.InitConfig()
-	ws.Add(5)
+
+	// start tasks
+	ws.Add(6)
 	go cron.UpdateBlocks(ws)
 	go cron.ProcessFailedSyncBlocks(ws)
 	go cron.UpdateTxns(ws)
 	go cron.UpdateBlockPay(ws)
 	go cron.UpdateAccounts(ws)
+	go cron.CheckRestart(ws)
 	ws.Wait()
 }
