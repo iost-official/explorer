eb1c1e3e7c6f733d9097f33086a8053e6020c6af
Pana <pan.wang@theseus.app>
2018-09-14 14:18:11 +0800

    update address

--- backend/model/blkchain/config.go
+++ backend/model/blkchain/config.go
@@ -2,6 +2,7 @@ package blkchain
 
 const (
 	RPCAddress         = "13.237.151.211:30002"
+	//RPCAddress         = "47.75.223.44:30302"
 	SystemContract     = "iost.system"
 	SystemTransferFunc = "Transfer"
 )
