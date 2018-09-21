0574f0d7e99428a207d6a7228c0998ff87ab526a
pengke <pengkedora@gmail.com>
2018-09-22 03:13:43 +0800

    add mark index

--- backend/model/db/config.go
+++ backend/model/db/config.go
@@ -48,6 +48,7 @@ func InitConfig() {
 	}
 	err = colTx.EnsureIndexKey("hash")
 	err = colTx.EnsureIndexKey("blockNumber")
+	err = colTx.EnsureIndexKey("mark")
 	colBlock, err := GetCollection(CollectionBlocks)
 	if err != nil {
 		log.Fatalln("Block collection create index, get collection error", err)
