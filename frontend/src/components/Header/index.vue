a30fd8bbb30dde01d37b8a2e4097eae0a689f0c3
sswsdsn <hrblhf08@gmail.com>
2019-02-12 20:22:13 +0800

    remove useless code

--- frontend/src/components/Header/index.vue
+++ frontend/src/components/Header/index.vue
@@ -18,13 +18,6 @@
             <router-link to="/txs">TRANSACTION</router-link>
           </li>
 
-          <!-- <li class="my-nav-item" v-show="!isShow" :class="{active: currentColor == 3}">
-            <router-link to="/accounts">ACCOUNTS</router-link>
-          </li> -->
-          <!-- <li class="my-nav-item" v-show="!isShow" :class="{active: currentColor == 4}">
-            <router-link to="/applyIOST">Create MainNet Account</router-link>
-          </li> -->
-
           <li class="my-nav-item searchActive" v-show="isShow">
             <input type="text" placeholder="Search..."
                    @click.stop="" v-model.trim="searchInput" @keydown.enter="searchData">
